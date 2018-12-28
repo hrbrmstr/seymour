29a99b5551474b2b35d60d9a3ade512ed74e4675
boB Rudis <bob@rud.is>
2018-12-28 07:14:37 -0500

    R package repo initialization complete

---
+++ R/seymour-package.R
@@ -0,0 +1,12 @@
+#' ...
+#' 
+#' - URL: <https://gitlab.com/hrbrmstr/seymour>
+#' - BugReports: <https://gitlab.com/hrbrmstr/seymour/issues>
+#' 
+#' @md
+#' @name seymour
+#' @docType package
+#' @author Bob Rudis (bob@@rud.is)
+#' @import httr
+#' @importFrom jsonlite fromJSON
+NULL
